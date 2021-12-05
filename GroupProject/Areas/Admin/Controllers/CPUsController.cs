91a5281d59ea7761cd6815443f68d7ec98524f73
Daddy-Gats <charalamposgatsakos@gmail.com>
2021-12-05 14:57:30 +0200

    Cpus Controller

--- GroupProject/Areas/Admin/Controllers/CPUsController.cs
+++ GroupProject/Areas/Admin/Controllers/CPUsController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Core.Entities;
+using DataAccess.Core.Interfaces;
 using DataAccess.Persistence;
 using System.Data.Entity;
 using System.Linq;
@@ -8,25 +9,29 @@ using System.Web.Mvc;
 namespace GroupProject.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
-    public class CPUsController : Controller
+    public class CpusController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-
-        // GET: Admin/CPUs
-        public ActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+        public CpusController(IUnitOfWork unitOfWork)
         {
-            var cPUs = db.CPUs.Include(c => c.Company);
-            return View(cPUs.ToList());
+            _unitOfWork = unitOfWork;
         }
 
-        // GET: Admin/CPUs/Details/5
+        // GET: Admin/Cpus
+        public ActionResult Index()
+        {
+            var Cpus = _unitOfWork.Cpus.GetAll();
+            return View(Cpus.ToList());
+        }
+
+        // GET: Admin/Cpus/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CPU cPU = db.CPUs.Find(id);
+            CPU cPU = _unitOfWork.Cpus.GetById(id);
             if (cPU == null)
             {
                 return HttpNotFound();
@@ -34,72 +39,72 @@ namespace GroupProject.Areas.Admin.Controllers
             return View(cPU);
         }
 
-        // GET: Admin/CPUs/Create
+        // GET: Admin/Cpus/Create
         public ActionResult Create()
         {
-            ViewBag.CompanyID = new SelectList(db.Companies, "ID", "Name");
+            ViewBag.CompanyID = new SelectList(_unitOfWork.Companies.GetAll(), "ID", "Name");
             return View();
         }
 
-        // POST: Admin/CPUs/Create
+        // POST: Admin/Cpus/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,CompanyID,Socket,Model,Cores,Threads,Frequency,Watt,Thumbnail,Price")] CPU cPU)
+        public ActionResult Create(CPU cPU)
         {
             if (ModelState.IsValid)
             {
-                db.CPUs.Add(cPU);
-                db.SaveChanges();
+                _unitOfWork.Cpus.Create(cPU);
+                _unitOfWork.Complete();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.CompanyID = new SelectList(db.Companies, "ID", "Name", cPU.CompanyID);
+            ViewBag.CompanyID = new SelectList(_unitOfWork.Companies.GetAll(), "ID", "Name", cPU.CompanyID);
             return View(cPU);
         }
 
-        // GET: Admin/CPUs/Edit/5
+        // GET: Admin/Cpus/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CPU cPU = db.CPUs.Find(id);
+            CPU cPU = _unitOfWork.Cpus.GetById(id);
             if (cPU == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.CompanyID = new SelectList(db.Companies, "ID", "Name", cPU.CompanyID);
+            ViewBag.CompanyID = new SelectList(_unitOfWork.Companies.GetAll(), "ID", "Name", cPU.CompanyID);
             return View(cPU);
         }
 
-        // POST: Admin/CPUs/Edit/5
+        // POST: Admin/Cpus/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,CompanyID,Socket,Model,Cores,Threads,Frequency,Watt,Thumbnail,Price")] CPU cPU)
+        public ActionResult Edit(CPU cPU)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(cPU).State = EntityState.Modified;
-                db.SaveChanges();
+                _unitOfWork.Cpus.Update(cPU);
+                _unitOfWork.Complete();
                 return RedirectToAction("Index");
             }
-            ViewBag.CompanyID = new SelectList(db.Companies, "ID", "Name", cPU.CompanyID);
+            ViewBag.CompanyID = new SelectList(_unitOfWork.Companies.GetAll(), "ID", "Name", cPU.CompanyID);
             return View(cPU);
         }
 
-        // GET: Admin/CPUs/Delete/5
+        // GET: Admin/Cpus/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CPU cPU = db.CPUs.Find(id);
+            CPU cPU = _unitOfWork.Cpus.GetById(id);
             if (cPU == null)
             {
                 return HttpNotFound();
@@ -107,14 +112,14 @@ namespace GroupProject.Areas.Admin.Controllers
             return View(cPU);
         }
 
-        // POST: Admin/CPUs/Delete/5
+        // POST: Admin/Cpus/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            CPU cPU = db.CPUs.Find(id);
-            db.CPUs.Remove(cPU);
-            db.SaveChanges();
+            CPU cPU = _unitOfWork.Cpus.GetById(id);
+            _unitOfWork.Cpus.Delete(id);
+            _unitOfWork.Complete();
             return RedirectToAction("Index");
         }
 
@@ -122,7 +127,7 @@ namespace GroupProject.Areas.Admin.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                _unitOfWork.Dispose();
             }
             base.Dispose(disposing);
         }
