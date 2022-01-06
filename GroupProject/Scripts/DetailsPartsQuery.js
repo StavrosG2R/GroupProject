c1ddcf6b67d62e3db2047d513bd899d2488985a5
Filippos Amitsis <filippose36@gmail.com>
2022-01-06 21:33:10 +0200

    Company Names added to Builds Create View

--- GroupProject/Scripts/DetailsPartsQuery.js
+++ GroupProject/Scripts/DetailsPartsQuery.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             success: function (data) {
                 $('#CasesLabel').empty();
                 console.log(data);
-                $('#CasesLabel').append('<p>' + '<strong>Case Model: </strong>' + data.model + '<br/>' + '<strong>Case Size: </strong>' + data.size + '<br />' +
+                $('#CasesLabel').append('<p>' + '<strong>Case Model: </strong>' + data.company.name + ' ' + data.model + '<br/>' + '<strong>Case Size: </strong>' + data.size + '<br />' +
                     '<strong>Number of Fans: </strong>' + data.numberOfFans + '<br />' + '<strong>Case Price: </strong>' + data.price +  "€" + '</p>')
             }
         })
@@ -26,7 +26,7 @@ $(document).ready(function () {
             success: function (data) {
                 $('#CPULabel').empty();
                 console.log(data);
-                $('#CPULabel').append('<p>' + '<strong>CPU Model: </strong>' + data.model + '<br/>' + '<strong>Frequency: </strong>' + data.frequency + '<br />' +
+                $('#CPULabel').append('<p>' + '<strong>CPU Model: </strong>' + data.company.name + ' ' + data.model + '<br/>' + '<strong>Frequency: </strong>' + data.frequency + '<br />' +
                     '<strong>Cores: </strong>' + data.cores + '<br />' + '<strong>Case Price: </strong>' + data.price + " €" + '</p>')
             }
         })
@@ -41,7 +41,7 @@ $(document).ready(function () {
             success: function (data) {
                 $('#MotherboardLabel').empty();
                 console.log(data);
-                $('#MotherboardLabel').append('<p>' + '<strong>Motherboard Model: </strong>' + data.model + '<br/>' + '<strong>Chipset: </strong>' + data.chipset + '<br />' +
+                $('#MotherboardLabel').append('<p>' + '<strong>Motherboard Model: </strong>' + data.company.name + ' ' + data.model + '<br/>' + '<strong>Chipset: </strong>' + data.chipset + '<br />' +
                     '<strong>Size: </strong>' + data.size + '<br />' + '<strong>Motherboard Price: </strong>' + data.price + " €" + '</p>')
             }
         })
@@ -56,7 +56,7 @@ $(document).ready(function () {
             success: function (data) {
                 $('#RamLabel').empty();
                 console.log(data);
-                $('#RamLabel').append('<p>' + '<strong>RAM Model: </strong>' + data.model + '<br/>' + '<strong>Frequency: </strong>' + data.frequency + '<br />' +
+                $('#RamLabel').append('<p>' + '<strong>RAM Model: </strong>' + data.company.name + ' ' + data.model + '<br/>' + '<strong>Frequency: </strong>' + data.frequency + '<br />' +
                     '<strong>Storage Size: </strong>' + data.storage + ' GB' + '<br />' + '<strong>RAM Price: </strong>' + data.price + " €" + '</p>')
             }
         })
@@ -71,7 +71,7 @@ $(document).ready(function () {
             success: function (data) {
                 $('#GpuLabel').empty();
                 console.log(data);
-                $('#GpuLabel').append('<p>' + '<strong>GPU Model: </strong>' + data.model + '<br/>' + '<strong>Memory: </strong>' + data.vram + ' GB' + '<br />' +
+                $('#GpuLabel').append('<p>' + '<strong>GPU Model: </strong>' + data.company.name + ' ' + data.model + '<br/>' + '<strong>Memory: </strong>' + data.vram + ' GB' + '<br />' +
                     '<strong>Power Consumption: </strong>' + data.watt + ' Watt' + '<br />' + '<strong>GPU Price: </strong>' + data.price + " €" + '</p>')
             }
         })
@@ -101,7 +101,7 @@ $(document).ready(function () {
             success: function (data) {
                 $('#StorageLabel').empty();
                 console.log(data);
-                $('#StorageLabel').append('<p>' + '<strong>Storage Model: </strong>' + data.model + '<br/>' + '<strong>Capacity: </strong>' + data.capacity + ' GB' + '<br />' +
+                $('#StorageLabel').append('<p>' + '<strong>Storage Model: </strong>' + data.company.name + ' ' + data.model + '<br/>' + '<strong>Capacity: </strong>' + data.capacity + ' GB' + '<br />' +
                     '<strong>Read Speed: </strong>' + data.readSpeed + ' Mb/s' + '<br />' + '<strong>Write Speed: </strong>' + data.writeSpeed + ' Mb/s' + '<br />' +
                     '<strong>PSU Price: </strong>' + data.price + " €" + '</p>')
             }
